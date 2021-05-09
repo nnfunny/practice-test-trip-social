053040f41c64276ae80ce30316b1968cc4150bc2
Nam Nguyen <nguyenvietnam2401@gmail.com>
2021-05-09 10:21:33 +1000

    fix: downgrade node syntax to 0.10.26

--- question-4/controllers/suggestion.controller.js
+++ question-4/controllers/suggestion.controller.js
@@ -1,67 +1,42 @@
-const {
-  extractLocations,
-  calculateLocationScore,
-  calculateLocationScore2,
-  matchWithName,
-  sortInDescendingOrder,
-} = require("../utils/suggestion.util");
-const path = require("path");
+var suggestionUtil = require("../utils/suggestion.util"),
+  extractLocations = suggestionUtil.extractLocations,
+  calculateLocationScore = suggestionUtil.calculateLocationScore,
+  calculateLocationScore2 = suggestionUtil.calculateLocationScore2,
+  matchWithName = suggestionUtil.matchWithName,
+  sortInDescendingOrder = suggestionUtil.sortInDescendingOrder;
 
-// const suggestions = {
-//   suggestions: [
-//     {
-//       name: "London, ON, Canada",
-//       latitude: "42.98339",
-//       longitude: "-81.23304",
-//       score: 0.9,
-//     },
-//     {
-//       name: "London, OH, USA",
-//       latitude: "39.88645",
-//       longitude: "-83.44825",
-//       score: 0.5,
-//     },
-//     {
-//       name: "London, KY, USA",
-//       latitude: "37.12898",
-//       longitude: "-84.08326",
-//       score: 0.5,
-//     },
-//     {
-//       name: "Londontowne, MD, USA",
-//       latitude: "38.93345",
-//       longitude: "-76.54941",
-//       score: 0.3,
-//     },
-//   ],
-// };
+var path = require("path");
+
 /**
  * Get suggestions for large cities
  * @public
  */
-exports.get = (req, res) => {
-  const tsvFile = path.join(__dirname, "../data/cities_canada-usa.tsv");
-  const locations = extractLocations(tsvFile);
-  let suggestions = [];
-  let { q, latitude, longitude } = req.query;
-
+exports.get = function (req, res) {
+  var tsvFile = path.join(__dirname, "../data/cities_canada-usa.tsv");
+  var locations = extractLocations(tsvFile);
+  var suggestions = [];
+  var query = req.query,
+      q = query.q,
+      latitude = query.latitude,
+      longitude = query.longitude;
   latitude = latitude ? latitude : 0;
   longitude = longitude ? longitude : 0;
 
-  locations.forEach((location) => {
-    const score1 = calculateLocationScore(location, latitude, longitude);
-    const score2 = calculateLocationScore2(location, latitude, longitude);
-    let score = (score1 + score2) / 2;
+  locations.forEach(function (location) {
+    var score1 = calculateLocationScore(location, latitude, longitude);
+    var score2 = calculateLocationScore2(location, latitude, longitude);
+    var score = (score1 + score2) / 2;
     score = +score.toFixed(1);
-    const name = location.name;
+    var name = location.name;
 
     if (score !== 0.0 && matchWithName(q, name)) {
-      let suggestion = { ...location, score };
-      suggestions.push(suggestion);
+      location.score = score;
+      suggestions.push(location);
     }
   });
 
   suggestions.sort(sortInDescendingOrder);
-
-  res.json({ suggestions });
+  res.json({
+    suggestions: suggestions,
+  });
 };
--- question-4/utils/suggestion.util.js
+++ question-4/utils/suggestion.util.js
@@ -1,26 +1,30 @@
-const fs = require('fs');
-const { MAX_DISTANCE } = require('./constants');
+"use strict";
 
+var fs = require("fs");
+
+var constants = require("./constants"),
+  MAX_DISTANCE = constants.MAX_DISTANCE;
 /**
- * Haversine formula: 	
+ * Haversine formula:
  *    a = sin²(Δφ/2) + cos φ1 ⋅ cos φ2 ⋅ sin²(Δλ/2)
  *    c = 2 ⋅ atan2( √a, √(1−a) )
  *    d = R ⋅ c
  * Reference: http://www.movable-type.co.uk/scripts/latlong.html
  */
-function calculateDistanceFormLatLonInKm(lat1, lon1, lat2, lon2) {
-  const R = 6371; // Earth's Radius in Km
-  const lat1InRad = convertToRad(lat1);
-  const lat2InRad = convertToRad(lat2);
-  const deltaLatInRad = convertToRad(lat2 - lat1);
-  const deltaLonInRad = convertToRad(lon2 - lon1);
-  
-  const a = Math.sin(deltaLatInRad / 2) * Math.sin(deltaLatInRad / 2) +
-            Math.cos(lat1InRad) * Math.cos(lat2InRad) *
-            Math.sin(deltaLonInRad / 2) * Math.sin(deltaLonInRad / 2);
-  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-  const d = R * c;
 
+function calculateDistanceFormLatLonInKm(lat1, lon1, lat2, lon2) {
+  var R = 6371; // Earth's Radius in Km
+
+  var lat1InRad = convertToRad(lat1);
+  var lat2InRad = convertToRad(lat2);
+  var deltaLatInRad = convertToRad(lat2 - lat1);
+  var deltaLonInRad = convertToRad(lon2 - lon1);
+  var a =
+    Math.sin(deltaLatInRad / 2) * Math.sin(deltaLatInRad / 2) +
+    Math.cos(lat1InRad) * Math.cos(lat2InRad) *
+    Math.sin(deltaLonInRad / 2) * Math.sin(deltaLonInRad / 2);
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  var d = R * c;
   return d;
 }
 
@@ -29,61 +33,62 @@ function convertToRad(deg) {
 }
 
 function calculateLocationScore(location, lat, lon) {
-  const distanceInKm = calculateDistanceFormLatLonInKm(
+  var distanceInKm = calculateDistanceFormLatLonInKm(
     location.latitude,
     location.longitude,
     lat,
-    lon,
+    lon
   );
-  let score = MAX_DISTANCE - distanceInKm;
-
+  var score = MAX_DISTANCE - distanceInKm;
   score = score > 0 ? Math.round(score) / MAX_DISTANCE : 0;
   score = +score.toFixed(1);
-
   return score;
 }
 
 function calculateLocationScore2(location, latitude, longitude) {
-  const lat = Math.abs(location.latitude - latitude);
-  const long = Math.abs(location.longitude - longitude);
-  let score = 10 - (lat + long) / 2;
+  var lat = Math.abs(location.latitude - latitude);
+
+  var _long = Math.abs(location.longitude - longitude);
+
+  var score = 10 - (lat + _long) / 2;
   score = score > 0 ? Math.round(score) / 10 : 0;
   return score;
 }
 
 function extractLocations(tsvFile) {
-  let locations = []
+  var locations = [];
 
   try {
-    const data = fs.readFileSync(tsvFile, 'utf-8');
-
-    let rows = data.split(/\n/g);
-    rows.forEach(row => {
-      let location = row.split(/\t/g);
-      let city = location[1] ? location[1] : "";
-      let admin1 = location[7] ? `${location[10]}` : "";
-      let country = location[8] ? `${location[8]}` : "";
+    var data = fs.readFileSync(tsvFile, "utf-8");
+    var rows = data.split(/\n/g);
+    rows.forEach(function (row) {
+      var location = row.split(/\t/g);
+      var city = location[1] ? location[1] : "";
+      var admin1 = location[7] ? "".concat(location[10]) : "";
+      var country = location[8] ? "".concat(location[8]) : "";
       country = country === "US" ? "USA" : "Canada";
-      let name = `${city}, ${admin1}, ${country}`;
-      let latitude = +location[4];
-      let longitude = +location[5];
-
+      var name = "".concat(city, ", ").concat(admin1, ", ").concat(country);
+      var latitude = +location[4];
+      var longitude = +location[5];
 
       if (name && latitude && longitude) {
-        location = { name, latitude, longitude }
+        location = {
+          name: name,
+          latitude: latitude,
+          longitude: longitude,
+        };
         locations.push(location);
       }
     });
   } catch (err) {
     console.error(err);
   }
-  
+
   return locations;
 }
 
 function matchWithName(keyword, name) {
-  let rgx = new RegExp(keyword, "gi");
-
+  var rgx = new RegExp(keyword, "gi");
   return rgx.test(name);
 }
 
@@ -92,10 +97,10 @@ function sortInDescendingOrder(suggestion1, suggestion2) {
 }
 
 module.exports = {
-  calculateDistanceFormLatLonInKm,
-  calculateLocationScore,
-  calculateLocationScore2,
-  extractLocations,
-  matchWithName,
-  sortInDescendingOrder,
+  calculateDistanceFormLatLonInKm: calculateDistanceFormLatLonInKm,
+  calculateLocationScore: calculateLocationScore,
+  calculateLocationScore2: calculateLocationScore2,
+  extractLocations: extractLocations,
+  matchWithName: matchWithName,
+  sortInDescendingOrder: sortInDescendingOrder,
 };
